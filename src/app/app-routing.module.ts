bc5656f4047aba2e55867e9841cdaf87fa227f62
Oleg <w.shevchuk.o@gmail.com>
2023-09-11 18:11:38 +0300

    Prepare for Github pages deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { AuthGuard } from './guards/auth.guard';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./guards/auth.guard";
 
 const routes: Routes = [
   {
-    path: 'secure',
-    loadChildren: () => import('./modules/secure/secure.module').then( mod => mod.SecureModule)},
-  { path: 'content',
-    loadChildren: () => import('./modules/content/content.module').then( mod => mod.ContentModule), canActivate: [AuthGuard]},
+    path: "secure",
+    loadChildren: () =>
+      import("./modules/secure/secure.module").then((mod) => mod.SecureModule),
+  },
   {
-    path: '',
-    loadChildren: () => import('./modules/content/content.module').then( mod => mod.ContentModule), canActivate: [AuthGuard]}
+    path: "",
+    loadChildren: () =>
+      import("./modules/content/content.module").then(
+        (mod) => mod.ContentModule
+      ),
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: "legacy" })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/modules/secure/components/access-check/access-check.component.ts
+++ src/app/modules/secure/components/access-check/access-check.component.ts
@@ -1,45 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { MyErrorStateMatcher } from '../../../../helpers/errorStateMatcher';
-import { environment } from '../../../../../environments/environment';
-import { MessageService } from 'primeng/api';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { MessageService } from "primeng/api";
+import { environment } from "../../../../../environments/environment";
+import { MyErrorStateMatcher } from "../../../../helpers/errorStateMatcher";
 
 @Component({
-  selector: 'app-access-check',
-  templateUrl: './access-check.component.html',
-  styleUrls: ['./access-check.component.css']
+  selector: "app-access-check",
+  templateUrl: "./access-check.component.html",
+  styleUrls: ["./access-check.component.css"],
 })
 export class AccessCheckComponent implements OnInit {
   public passwordForm: FormGroup;
   public matcher = new MyErrorStateMatcher();
   private password = environment.applicationAccess;
 
-  constructor(
-    private router: Router,
-    private messageService: MessageService,
-  ) { }
+  constructor(private router: Router, private messageService: MessageService) {}
 
   public count = 0;
 
   ngOnInit() {
     this.passwordForm = new FormGroup({
-      'password': new FormControl('', [Validators.required, Validators.minLength(3)])
+      password: new FormControl("", [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
     });
   }
 
   public getAccess() {
-    const usersPass = this.passwordForm.get('password').value;
+    const usersPass = this.passwordForm.get("password").value;
     if (this.password === usersPass && this.count < 3) {
-      localStorage.setItem('доступ', 'дозволено');
-      this.messageService.add({severity: 'success', summary: 'Access allow', detail: 'thanks'});
-      this.router.navigate([`/content/skills`]);
+      localStorage.setItem("доступ", "дозволено");
+      this.messageService.add({
+        severity: "success",
+        summary: "Access allow",
+        detail: "thanks",
+      });
+      this.router.navigate([`/skills`]);
     } else if (this.password !== usersPass && this.count < 3) {
       this.count += 1;
       this.passwordForm.reset();
-      this.messageService.add({severity: 'warn', summary: 'Access is denied', detail: 'Wrong password'});
+      this.messageService.add({
+        severity: "warn",
+        summary: "Access is denied",
+        detail: "Wrong password",
+      });
     } else {
-      this.messageService.add({severity: 'error', summary: 'Access closed', detail: 'Wrong password 3 times.'});
+      this.messageService.add({
+        severity: "error",
+        summary: "Access closed",
+        detail: "Wrong password 3 times.",
+      });
       this.passwordForm.reset();
     }
   }
